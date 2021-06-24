e5b7774a59d623a5411dbe520af741215890111e
Asha Hakari <asha.hakari@gmail.com>
2021-06-23 20:02:39 -0700

    Added Songs
    Added songs

--- FirstAssignment/MainPage.xaml.cs
+++ FirstAssignment/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FirstAssignment.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,25 +24,71 @@ namespace FirstAssignment
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Music> Songs;
+        private List<LeftMenuItem> LeftMenuItems;
         public MainPage()
         {
             this.InitializeComponent();
             BackButton.Visibility = Visibility.Collapsed;
+            Songs = new ObservableCollection<Music>();
+            MusicManager.GetAllSongs(Songs);
+
+            LeftMenuItems = new List<LeftMenuItem>
+            {
+                new LeftMenuItem
+                {
+                    Genre = MusicGenre.Classical,
+                    IconFile = "/Assets/Icons/classical.png"
+                },
+
+                new LeftMenuItem
+                {
+                    Genre = MusicGenre.Jazz,
+                    IconFile = "/Assets/Icons/jazz.png"
+                },
+
+                new LeftMenuItem
+                {
+                    Genre = MusicGenre.Pop,
+                    IconFile = "/Assets/Icons/pop.png"
+                },
+                new LeftMenuItem
+                {
+                    Genre = MusicGenre.Rock,
+                    IconFile = "/Assets/Icons/rock.png"
+                }
+
+
+            };
+           
+
         }
 
         private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MusicSplitView.IsPaneOpen = !MusicSplitView.IsPaneOpen;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MusicManager.GetAllSongs(Songs);
+            BackButton.Visibility = Visibility.Collapsed;
+            CategoryTextBlock.Text = "All Music";
         }
 
         private void MusicGridView_ItemClick(object sender, ItemClickEventArgs e)
         {
+            var song = (Music)e.ClickedItem;
+            MusicLibraryMediaElement.Source = new Uri(this.BaseUri, song.AudioFile);
+        }
 
+        private void MenuItemListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (LeftMenuItem) e.ClickedItem;
+
+            MusicManager.GetAllSongsByGenre(Songs, menuItem.Genre);
+            BackButton.Visibility = Visibility.Visible;
+            CategoryTextBlock.Text = menuItem.Genre.ToString();
         }
     }
 }
---
+++ FirstAssignment/Model/Music.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAssignment.Model
+{
+    public enum MusicGenre
+    {
+        Rock,
+        Jazz,
+        Pop,
+        Hiphop,
+        Folk,
+        Country,
+        Classical
+    }
+    public class Music
+    {
+        public string Name { get; set; }
+        public MusicGenre Genre { get; set; }
+        public string CoverArtFile { get; set; }
+        public string AudioFile { get; set; }
+        public string Artist { get; set; }
+
+        public Music(string name, MusicGenre genre)
+        {
+            Name = name;
+            Genre = genre;
+            CoverArtFile = $"/Assets/CoverArt/{genre}/{name}.jpg";
+            AudioFile = $"/Assets/SongFiles/{genre}/{name}.mp3";
+        }
+    }
+}
